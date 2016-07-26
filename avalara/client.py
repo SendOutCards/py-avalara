cd9c690753944d76f13a6d16100ce9663c6326e9
Daniel Hanson <daniel.hanson@sendoutcards.com>
2016-07-26 16:59:13 -0600

    added GetTaxRequest object which inherits from Avalara Client object

--- avalara/client.py
+++ avalara/client.py
@@ -11,18 +11,10 @@ DEFAULT_BASE_URL = 'https://development.avalara.net/1.0/'
 
 class Avalara(object):
 
-    def __init__(self, account_number=None, license_key=None, base_url=DEFAULT_BASE_URL, **kwargs):
+    def __init__(self, account_number=None, license_key=None, **kwargs):
         self.account_number = account_number or os.getenv('AVALARA_ACCOUNT_NUMBER')
         self.license_key = license_key or os.getenv('AVALARA_LICENSE_KEY')
-        self.base_url = os.getenv('AVALARA_BASE_URL') or base_url
-        self.get_tax_request = {
-            "CustomerCode": kwargs.get('user_id', 'TEMPCODE'),
-            "CompanyCode": "SOC",
-            "DetailLevel": "Document",
-            "CurrencyCode": "USD",
-            "Addresses": [],
-            "Lines": []
-        }
+        self.base_url = os.getenv('AVALARA_BASE_URL') or DEFAULT_BASE_URL
 
     @property
     def _auth_token(self):
@@ -46,20 +38,6 @@ class Avalara(object):
 
         return urljoin(self.base_url, endpoint)
 
-    def _get_tax(self):
-        url = self._build_url('tax/get')
-        return self._make_request('post', url, json=self.get_tax_request)
-
-    def get_tax(self):
-        self.get_tax_request['DocType'] = 'SalesOrder'
-        self.get_tax_request['Commit'] = 'false'
-        return self._get_tax()
-
-    def commit_tax(self):
-        self.get_tax_request['DocType'] = 'SalesInvoice'
-        self.get_tax_request['Commit'] = 'true'
-        return self._get_tax()
-
     def validate_address(self, address1, country, address2='',
                          address3='', city='', region='', postal_code=''):
 
@@ -74,6 +52,43 @@ class Avalara(object):
             'PostalCode': postal_code
         }
         return self._make_request('get', url, params=request_data)
+
+
+    def estimate_tax(self, latitude, longitude, sale_amount):
+        url = self._build_url('{{longitude}},{{latitude}}/tax/estimate?{{saleamount}}',
+                              latitude=latitude, longitude=longitude, saleamount=sale_amount)
+        return self._make_request('get', url)
+
+    def void_document(self, cancel_tax_request):
+        url = self._build_url('tax/cancel')
+        return self._make_request('post', url, json=cancel_tax_request)
+
+
+class GetTaxRequest(Avalara):
+    def __init__(self, account_number=None, license_key=None, **kwargs):
+        super(GetTaxRequest, self).__init__(account_number=account_number, license_key=license_key, **kwargs)
+        self.get_tax_request = {
+            "CustomerCode": kwargs.get('user_id', 'TEMPCODE'),
+            "CompanyCode": "SOC",
+            "DetailLevel": "Document",
+            "CurrencyCode": "USD",
+            "Addresses": [],
+            "Lines": []
+        }
+
+    def _get_tax(self):
+        url = self._build_url('tax/get')
+        return self._make_request('post', url, json=self.get_tax_request)
+
+    def get_tax(self):
+        self.get_tax_request['DocType'] = 'SalesOrder'
+        self.get_tax_request['Commit'] = 'false'
+        return self._get_tax()
+
+    def commit_tax(self):
+        self.get_tax_request['DocType'] = 'SalesInvoice'
+        self.get_tax_request['Commit'] = 'true'
+        return self._get_tax()
 
     def add_line_item(self, address_number, tax_code, item_code, qty, price, desc=''):
         """
@@ -106,13 +121,4 @@ class Avalara(object):
             'PostalCode': kwargs.get('postal_code', '')
         })
         return address_number
-
-    def estimate_tax(self, latitude, longitude, sale_amount):
-        url = self._build_url('{{longitude}},{{latitude}}/tax/estimate?{{saleamount}}',
-                              latitude=latitude, longitude=longitude, saleamount=sale_amount)
-        return self._make_request('get', url)
-
-    def void_document(self, cancel_tax_request):
-        url = self._build_url('tax/cancel')
-        return self._make_request('post', url, json=cancel_tax_request)
 
