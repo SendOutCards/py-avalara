a523610d3f96d5dae8569e7abcdb167e96165a4e
Daniel Hanson <daniel.hanson@sendoutcards.com>
2016-08-26 12:53:15 -0600

    constants for shipping and handling item codes

--- avalara/__init__.py
+++ avalara/__init__.py
@@ -1,9 +1,11 @@
 from .client import Avalara
 from .constants import (
     DEFAULT_TAX_CODE,
+    HANDLING_ITEM_CODE,
     HANDLING_TAX_CODE,
     NON_TAXABLE_TAX_CODE,
     SHIPPING_TAX_CODE,
+    SHIPPING_ITEM_CODE,
 )
 from .models import GetTaxRequest
 
@@ -12,7 +14,9 @@ __all__ = [
     'Avalara',
     'GetTaxRequest',
     'DEFAULT_TAX_CODE',
+    'HANDLING_ITEM_CODE',
     'HANDLING_TAX_CODE',
     'NON_TAXABLE_TAX_CODE',
+    'SHIPPING_ITEM_CODE',
     'SHIPPING_TAX_CODE',
 ]
